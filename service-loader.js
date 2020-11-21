2ec40409ba8d506ebbed2542031542f8ac766ecc
Malik Zharykov <cmalikz.h@gmail.com>
2020-11-21 17:53:28 +0600

    closes #1

---
+++ service-loader.js
@@ -0,0 +1,95 @@
+const _ = require('lodash');
+
+/**
+ * Recursive service loader
+ *
+ * @param {Object} container CoreDI main container
+ * @param {String} serviceName Service name
+ * @param {Boolean} forkMode Use fork function
+ * @param {String[]} dependencyStack Dependencies stack
+ * @return {Promise<void>}
+ */
+module.exports = async function loadService(container, serviceName,
+    forkMode, dependencyStack) {
+  if (!_.isFunction(container)) {
+    throw new Error('Container argument must be a function');
+  }
+
+  if (!_.isString(serviceName) || serviceName.length < 1) {
+    throw new Error('Invalid service name');
+  }
+
+  if (!_.isObject(container.schema) ||
+    !_.isObject(container.schema[container.containerName]) ||
+    !_.isArray(container.schema[container.containerName].loaders) ||
+    !_.isObject(container.services)) {
+    throw new Error('Invalid container given');
+  }
+
+  const serviceLoader = container.schema[container.containerName].loaders
+      .find((s) => s.name === serviceName);
+
+  if (!serviceLoader) {
+    throw new Error('Cannot find service loader for service: ' + serviceName);
+  }
+
+  if (!_.isObject(serviceLoader.config)) {
+    serviceLoader.config = {};
+  }
+
+  // Check service creation function
+  let createFuncName = 'create';
+
+  if (forkMode && _.isFunction(serviceLoader['fork'])) {
+    createFuncName = 'fork';
+  }
+
+  if (!_.isFunction(serviceLoader[createFuncName])) {
+    throw new Error(
+        'create() function not found in service loader: ' + serviceName);
+  }
+
+  // Merge configuration
+  if (_.isObject(container.schema[container.containerName].config) &&
+    _.isObject(container.schema[container.containerName].config[serviceName])) {
+    serviceLoader.config = _.defaultsDeep(
+        container.schema[container.containerName].config[serviceName],
+        serviceLoader.config);
+    container.schema[container.containerName].config[serviceName] = serviceLoader.config;
+  }
+
+  // Resolve dependencies
+  if ((_.isArray(serviceLoader.requires) && serviceLoader.requires.length > 0) ||
+    (_.isString(serviceLoader.requires))) {
+
+    const requires = (_.isString(serviceLoader.requires) ? [serviceLoader.requires]
+    : serviceLoader.requires);
+
+    for (const dependencyName of requires) {
+      if (container.services[dependencyName]) {
+        continue;
+      }
+
+      // Check for recursive dependency
+      if (dependencyStack.indexOf(dependencyName) !== -1) {
+        throw new Error(
+            'Dependency recursion detected. On service "' + serviceLoader.name +
+            '". Service requires "' + dependencyName + '" but that service ' +
+            ' requires "' + serviceLoader.name + '"');
+      }
+
+      dependencyStack.push(dependencyName);
+      await loadService(container, dependencyName, forkMode, dependencyStack);
+      dependencyStack.pop();
+    }
+  }
+
+  // Create service
+  const service = await serviceLoader[createFuncName](container);
+
+  if (!service) {
+    throw new Error(`Service ${serviceName} provides nothing`);
+  }
+
+  container.services[serviceName] = service;
+};
---
+++ test/service-loader.test.js
@@ -0,0 +1,188 @@
+const assert = require('assert');
+const sinon = require('sinon');
+
+const loadService = require('../service-loader');
+
+describe('Service loader test', function() {
+  let sandbox;
+
+  beforeEach(function() {
+    sandbox = sinon.createSandbox();
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
+  it('Invalid container object', async function() {
+    await assert.rejects(loadService(null, null, null, null), {
+      message: 'Container argument must be a function',
+    });
+
+    await assert.rejects(loadService(function() {}, null, null, null), {
+      message: 'Invalid service name',
+    });
+
+    await assert.rejects(loadService(function() {}, 'test', null, null), {
+      message: 'Invalid container given',
+    });
+
+    const f1 = function() {};
+    f1.schema = {};
+
+    await assert.rejects(loadService(f1, 'test', null, null), {
+      message: 'Invalid container given',
+    });
+
+    const f2 = function() {};
+    f2.schema = {
+      testContainer: {
+        loaders: {},
+      },
+    };
+    f2.containerName = 'testContainer';
+
+    await assert.rejects(loadService(f2, 'test', null, null), {
+      message: 'Invalid container given',
+    });
+
+    const f3 = function() {};
+    f3.schema = {
+      testContainer: {
+        loaders: [],
+      },
+    };
+    f3.containerName = 'testContainer';
+
+    await assert.rejects(loadService(f3, 'test', null, null), {
+      message: 'Invalid container given',
+    });
+  });
+
+  it('Invalid service loader object', async function() {
+    const container = function() {};
+    container.schema = {
+      test123: {
+        loaders: [
+          {
+            name: 'testx',
+          },
+          {
+            name: 'testy',
+            async fork() {},
+          },
+        ],
+      },
+    };
+
+    container.services = {};
+    container.containerName = 'test123';
+
+    await assert.rejects(loadService(container, 'invalid', null, null), {
+      message: 'Cannot find service loader for service: invalid',
+    });
+
+    await assert.rejects(loadService(container, 'testx', null, null), {
+      message: 'create() function not found in service loader: testx',
+    });
+
+    await assert.rejects(loadService(container, 'testx', true, null), {
+      message: 'create() function not found in service loader: testx',
+    });
+
+    await assert.rejects(loadService(container, 'testy', true, null), {
+      message: 'Service testy provides nothing',
+    });
+  });
+
+  it('Test configuration merge', async function() {
+    const container = function() {};
+    container.schema = {
+      test123: {
+        loaders: [
+          {
+            name: 'testx',
+            config: {
+              param1: 'abc',
+              param2: 'cde',
+            },
+            async create() {},
+          },
+        ],
+        config: {
+          testx: {
+            param1: 'fff',
+            param3: 'fgh',
+          },
+        },
+      },
+    };
+
+    container.services = {};
+    container.containerName = 'test123';
+
+    await assert.rejects(loadService(container, 'testx', false, null), {
+      message: 'Service testx provides nothing',
+    });
+
+    assert.deepStrictEqual(container.schema.test123.loaders[0].config, {
+      param1: 'fff',
+      param2: 'cde',
+      param3: 'fgh',
+    });
+  });
+
+  it('Dependency resolver test', async function() {
+    const container = function() {};
+    container.schema = {
+      test123: {
+        loaders: [
+          {
+            name: 'testx',
+            requires: 'testy',
+            async create() {},
+          },
+          {
+            name: 'testy',
+            requires: 'testx',
+            async create() {},
+          },
+          {
+            name: 'testz',
+            requires: ['testa', 'testb'],
+            async create() {
+              return {};
+            },
+          },
+          {
+            name: 'testa',
+            async create() {
+              return {};
+            },
+          },
+        ],
+      },
+    };
+
+    container.services = {
+      testb: {},
+    };
+    container.containerName = 'test123';
+
+    let depStack = [];
+
+    await assert.rejects(loadService(container, 'testx', false, depStack), {
+      message: 'Dependency recursion detected. On service "testx". ' +
+        'Service requires "testy" but that service  requires "testx"',
+    });
+
+    depStack = [];
+
+    await loadService(container, 'testz', false, depStack);
+
+    assert.strictEqual(depStack.length, 0);
+    assert.ok(container.services.testz);
+    assert.ok(container.services.testa);
+    assert.ok(container.services.testb);
+  });
+});
