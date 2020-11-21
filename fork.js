67020c82891c821db3a3a9c0c47279bac8c4c982
Malik Zharykov <cmalikz.h@gmail.com>
2020-11-21 20:05:44 +0600

    closes #5

---
+++ fork.js
@@ -0,0 +1,56 @@
+const _ = require('lodash');
+const providerInstance = require('./provider-instance');
+const serviceLoader = require('./service-loader');
+
+function mergeSchema(a, b) {
+  const sc = _.cloneDeep(a);
+
+  // merge loaders
+  if (_.isArray(b.loaders)) {
+    sc.loaders = _.unionBy(b.loaders, sc.loaders, 'name');
+  }
+
+  // config
+  if (_.isObject(b.config)) {
+    sc.config = sc.config || {};
+    _.defaults(sc.config, b.config);
+  }
+
+  return sc;
+}
+
+/**
+ * Create fork of current container
+ *
+ * @param {Object} schema Container schemd
+ * @param {String} containerName Name of container
+ * @return {Promise<Function>} Forked container
+ */
+module.exports = async function(schema, containerName) {
+  const parentSchema = this.schema[this.containerName];
+
+  const newSchema = {
+    [containerName]: mergeSchema(mergeSchema(parentSchema, this.schema[containerName] || {}), schema || {}),
+  };
+
+  const instance = providerInstance();
+  instance.containerName = containerName;
+  instance.schema = newSchema;
+  instance.services = {};
+  instance.parent = this;
+  instance.children = {};
+
+  this.children[containerName] = instance;
+
+  const dependencyStack = [];
+
+  for (const loader of newSchema[containerName].loaders) {
+    if (instance.services[loader.name]) {
+      continue;
+    }
+
+    await serviceLoader(instance, loader.name, false, dependencyStack);
+  }
+
+  return instance;
+};
---
+++ test/fork.test.js
@@ -0,0 +1,78 @@
+const assert = require('assert');
+const sinon = require('sinon');
+const proxyquire = require('proxyquire').noPreserveCache().noCallThru();
+
+describe('Coredi Builder test', async function() {
+  let sandbox;
+
+  beforeEach(function () {
+    sandbox = sinon.createSandbox();
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  describe('Provider with parent', async function() {
+    it('test with parent no scope with config', async function() {
+      const ParentProvider = {
+        containerName: 'testcc',
+        schema: {
+          testcc: {
+            loaders: [{a: 'b'}],
+            config: {
+              param1: 'qwe',
+              param2: 'rty',
+            },
+          },
+          testr: {
+            loaders: [
+              {
+                name: 'rrr',
+              },
+              {
+                name: 'rtt',
+              },
+              {
+                name: 'xxx',
+              },
+            ],
+            config: {
+              param1: 'uio',
+              param3: 'rtc',
+            },
+          },
+        },
+        children: {},
+      };
+
+      const serviceLoaderSpy = sandbox.spy();
+
+      ParentProvider.fork = proxyquire('../fork', {
+        './provider-instance': function() {
+          return function() {};
+        },
+        './service-loader': async function(provider) {
+          provider.services = {
+            rtt: {},
+          };
+          serviceLoaderSpy();
+        },
+      });
+
+      const provider = await ParentProvider.fork({
+        config: {
+          param3: 'qqc',
+          param4: 'tmnv',
+        },
+        loaders: [
+          {
+            name: 'test32x',
+          },
+        ],
+      }, 'testr');
+
+      assert.strictEqual(provider.schema.testr.config.param1, 'qwe');
+    });
+  });
+});
